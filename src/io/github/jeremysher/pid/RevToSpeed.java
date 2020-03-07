01c8a0ba478fceff54e01c5753f0ae728c7bc6af
jeremysher <jeremysher13@gmail.com>
2020-03-07 16:06:24 -0500

    made RevToSpeed

--- src/io/github/jeremysher/pid/RevToSpeed.java
+++ src/io/github/jeremysher/pid/RevToSpeed.java
@@ -6,29 +6,31 @@ import java.awt.event.MouseEvent;
 
 import io.github.jeremysher.animation.Animation;
 
-public class DriveDistance {
+public class RevToSpeed {
 
 	public static void main(String[] args) {
-		Animation a = new Animation(1000, 500, "PID Simulator") {
-			
+		Animation a = new Animation(1000, 500, "RevToSpeed") {
+
 			private final double wheelY = 250;
 			
-			private final double kS = 1;
-			private final double kV = 3;
-			private final double kA = 0.5;
+			private final double kS = 0.171;
+			private final double kV = 0.129;
+			private final double kA = 0.018;
 			
-			private double scale = 100; // pixels per meter
-			private double setpointX = 9; // meters
+			private double scale = 1000; // pixels per meter
+			private double setSpeed = 240; // rad/s
 			private double wheelX = 0.5; // m
 			private double radius = 0.1; // m
 			private double mass = 1.0; //kg
 			private double omega = 0.0; // rad/s
 			private double minOmega = 1; // rad/s (min angular velocity to move
+			private double theta = 0; //rad
+			private double maxAcc = 666; //rad/s^2
 			
-			private PIDController pidController = new PIDController(1000, 0, 100,
+			private PIDController pidController = new PIDController(1.977, 0, 0,
 					() -> getDeltaTime(),
-					() -> wheelX,
-					() -> setpointX
+					() -> omega,
+					() -> setSpeed
 			);
 			
 
@@ -38,7 +40,10 @@ public class DriveDistance {
 				g.fillOval((int)(wheelX * scale - radius * scale), (int)wheelY, (int)(2 * radius * scale), (int)(2 * radius * scale));
 				
 				g.setColor(Color.red);
-				g.drawRect((int)(setpointX * scale), 0, 0, 500);
+				g.fillOval((int)((wheelX * scale) + Math.cos(theta) * radius * scale - 5),
+						(int)(wheelY + radius * scale + Math.sin(theta) * radius * scale - 5),
+						10,
+						10);
 				
 			}
 
@@ -49,19 +54,21 @@ public class DriveDistance {
 				double voltage = pidController.calculate();
 				
 				double alpha = (Math.abs(voltage - kV * omega) > kS ? (voltage - kV * omega - kS * Math.signum(omega != 0 ? omega : voltage)) / kA : 0);
+				alpha = Math.min(alpha, maxAcc);
 				omega += alpha * dt;
-				double v = (Math.abs(omega) > minOmega || Math.abs(voltage) > kS ? omega * radius : 0);
-				wheelX += v * dt;
+				//omega *= 0.95;
+				omega = (Math.abs(omega) > minOmega || Math.abs(voltage) > kS ? omega : 0);
+				theta += omega * dt;
+				System.out.println(omega);
 			}
 			
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				setpointX = e.getX() / scale;
+				omega -= 100;
 			}
 			
 		};
-		
 		a.start();
-	}
 
+	}
 }
